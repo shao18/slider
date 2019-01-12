31a969f61e6e47e50dd07d0ef9a08a177a0cbe97
sasha <sasha@localhost.localdomain>
2019-01-13 00:58:24 +0300

    day2

---
+++ src/components/Main.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+const Repair = styled.div`
+  color: white;
+  font-size: 50px;
+  font-weight: 700;
+  span {
+     display: block;
+     font-size: 24px;
+     line-height: 30px;
+
+  }
+
+  
+
+`;
+const LightText = styled.div`
+  color: white;
+  font-size: 18px;
+  font-weight: 3000;
+  line-height: 28px;
+  margin-top: 54px;
+`
+
+const CallBtn = styled.button`
+  width: 247px;
+  height: 67px;
+  margin: 32px;
+  background-color: #ffa14b;
+  border-radius: 30px;
+
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: bold;
+  
+`
+class Main extends Component{
+
+	render () {
+
+		return (
+               <Row>
+                 <Col lg={5}>
+                 <Repair>
+                 Качественный ремонт
+iphone за 35 минут и гарантия 1 год
+                 </Repair>
+<LightText>Оставьте заявку на бесплатную диагностику без очереди,
+и получите защитное стекло, стоимостью 1000 рублей,
+с установкой в подарок!</LightText>
+<CallBtn/>
+                 </Col>
+                 <Col lg={6} lgOffset={1}>
+                 </Col>
+                </Row> 
+
+			)
+	}
+}
+
+export default Main;
+
--- src/index.js
+++ src/index.js
@@ -4,17 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function SayFullName(props){
-	return (
-		<div>
-		<h1>Мое имя {props.name}, фамилия {props.surname}</h1>
-		<a href={props.link}>ССылка на мой профиль</a>
-		</div>
-		)
 
-}
 
-ReactDOM.render(<SayFullName name="Вася" surname="Петров" link="http://vk.com/" />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
